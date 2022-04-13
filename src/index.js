e4052e2e24ab5ef26375effe802d089cfbce8266
Kyle Loney <kyleloney9109@gmail.com>
2022-04-12 19:49:06 -0500

    Color changes

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const myBox=document.getElementById('my-box');
+
+function setColor(color) {
+myBox.style.backgroundColor=color
+}
+
+function onClick() {
+setColor('blue');
+}
+
+myBox.addEventListener('click', onClick);
